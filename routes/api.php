a89744d0b3ab1a7a412e78bd94cd9f9608ce5be0
Lucas <gosgnach24lucas@gmail.com>
2025-04-11 13:58:58 +0200

    fix

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthenticationController::class, 'register']);
-Route::post('/login', [AuthenticationController::class, 'login']);
+Route::get('/login', [AuthenticationController::class, 'login']);
+//Route::post('/login', [AuthenticationController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/me', [AuthenticationController::class, 'me']);
