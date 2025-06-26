02d68f69ae65db08503aca883fee7f8bc97da2e4
howonearth <155461817+howonearth@users.noreply.github.com>
2025-06-26 07:57:41 +0200

    Added relations between Post Model and User Model for Saved  (#40)
    * Added relations between Post Model and User Model - Saved
    
    * Apply fixes from StyleCI
    
    * Added constraints for the pivot table saved
    
    ---------
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Post extends Model
 {
@@ -26,5 +27,11 @@ class Post extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function usersaved(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'saved')
+        ->withTimestamps();
     }
 }
