877d1fd7b21ed060fe17290e2934ce94e7109b35
howonearth <155461817+howonearth@users.noreply.github.com>
2025-07-01 14:21:23 +0200

    Added Comment Model and its relations to Post, User and Comment (#47)
    * Added Comment Model and its relations to Post, User and Comment
    
    * Apply fixes from StyleCI
    
    * Resolved Coderabbit Suggestions
    
    * Apply fixes from StyleCI
    
    ---------
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

---
+++ app/Models/Comment.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Comment extends Model
+{
+    /** @use HasFactory<\Database\Factories\CommentFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'comment',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function comment(): BelongsTo
+    {
+        return $this->belongsTo(Comment::class);
+    }
+
+    public function child_comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -75,4 +75,9 @@ class User extends Authenticatable
         return $this->belongsToMany(Post::class, 'shared')
         ->withTimestamps();
     }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
