468e37c406877a1c49e16f7c67025d54cfa02117
howonearth <155461817+howonearth@users.noreply.github.com>
2025-07-01 09:52:48 +0200

    Added Search Model, Rest Resource, Rest Controller, api routes for Search feature (#45)
    * Added Search feature
    
    * Apply fixes from StyleCI
    
    * Resolved coderabbit suggestion
    
    ---------
    
    Co-authored-by: StyleCI Bot <bot@styleci.io>

---
+++ app/Rest/Resources/SearchResource.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Rest\Resources;
+
+use App\Models\Search;
+use App\Rest\Resource;
+use Lomkit\Rest\Http\Requests\RestRequest;
+
+class SearchResource extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    public static $model = Search::class;
+
+    /**
+     * The exposed fields that could be provided.
+     *
+     * @param RestRequest $request
+     *
+     * @return array
+     */
+    public function fields(RestRequest $request): array
+    {
+        return [
+            'id',
+            'text',
+        ];
+    }
+
+    /**
+     * The exposed relations that could be provided.
+     *
+     * @param RestRequest $request
+     *
+     * @return array
+     */
+    public function relations(RestRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * The exposed scopes that could be provided.
+     *
+     * @param RestRequest $request
+     *
+     * @return array
+     */
+    public function scopes(RestRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * The exposed limits that could be provided.
+     *
+     * @param RestRequest $request
+     *
+     * @return array
+     */
+    public function limits(RestRequest $request): array
+    {
+        return [
+            10,
+            25,
+            50,
+        ];
+    }
+
+    /**
+     * The actions that should be linked.
+     *
+     * @param RestRequest $request
+     *
+     * @return array
+     */
+    public function actions(RestRequest $request): array
+    {
+        return [];
+    }
+
+    /**
+     * The instructions that should be linked.
+     *
+     * @param RestRequest $request
+     *
+     * @return array
+     */
+    public function instructions(RestRequest $request): array
+    {
+        return [];
+    }
+}
